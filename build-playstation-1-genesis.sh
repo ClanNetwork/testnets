8547ad210703dae03db8d69c33427e32b8f7f151
AmitKendler <amitkendler1@gmail.com>
2022-04-27 00:26:38 +0300

    Update script code

--- build-playstation-1-genesis.sh
+++ build-playstation-1-genesis.sh
@@ -5,6 +5,8 @@ CHAIN_ID=playstation-1
 VALIDATOR_INIT_COINS=100000000000000$DENOM
 FAUCET_INIT_COINS=50000000000000$DENOM
 ORACLE_INIT_COINS=1000000$DENOM
+GENTX_PATH="playstation-1"
+VALIDATORS_GENESIS_ALLOCATION=1000000000000$DENOM
 REQUIRED_VERSION="1.0.1-alpha"
 VERSION="$(cland version |  awk '{print $NF}')"
 SNAPSHOT_ATOM_STAKERS="snapshot_atom_9681900.json"
@@ -21,14 +23,11 @@ EXPORTED_CLAIM_ETH_RECORDS="exported-claim-eth-records.json"
 VALIDATOR_ADDRESS="clan10jq29ktde4xpges8ah0z4r48ywqsj7029u9f5c"
 ORACLE_ADDRESS="clan147m4eyj8ejcax2k5a96ag5yxan8884p37qnn9z"
 FAUCET_ADDRESS="clan1anl88fsxy2pucyxdxme8pmpmw779lvhhn0faks"
-GENTX_PATH="playstation-1"
-VALIDATORS_GENESIS_ALLOCATION=100000000000
 
 if [ "$VERSION" != "$REQUIRED_VERSION" ]; then
     echo "cland required $REQUIRED_VERSION, current $VERSION"
     exit 1
 fi
-
 
 rm -f ~/.clan/config/genesis.json
 rm -f ~/.clan/config/gentx/*
@@ -61,24 +60,17 @@ cland prepare-genesis testnet $CHAIN_ID $EXPORTED_CLAIM_RECORDS $EXPORTED_CLAIM_
 
 cland init testmoniker --chain-id $CHAIN_ID
 
-cland config chain-id $CHAIN_ID
-cland config keyring-backend test
-cland config output json
-
 cland add-genesis-account $VALIDATOR_ADDRESS $VALIDATOR_INIT_COINS
 cland add-genesis-account $ORACLE_ADDRESS $ORACLE_INIT_COINS
 cland add-genesis-account $FAUCET_ADDRESS $FAUCET_INIT_COINS
 
 echo "Processing gentxs"
 mkdir -p ~/.clan/config/gentx
-for i in $GENTX_PATH/gentx/*.json; do
+for i in $GENTX_PATH/gentxs/*.json; do
     echo $i
     cland add-genesis-account $(jq -r '.body.messages[0].delegator_address' $i) $VALIDATORS_GENESIS_ALLOCATION 
     cp $i ~/.clan/config/gentx/
 done
-
-cland keys add my-validator --keyring-backend test --recover
-cland gentx my-validator 95000000000000uclan --chain-id $CHAIN_ID --keyring-backend test
 
 cland collect-gentxs
 cland validate-genesis
@@ -86,5 +78,5 @@ cland validate-genesis
 cp ~/.clan/config/genesis.json $GENTX_PATH/pre-built.json
 jq -S -f normalize.jq  ~/.clan/config/genesis.json > $GENTX_PATH/genesis.json
 cp $GENTX_PATH/genesis.json ~/.clan/config/genesis.json
-sha256sum $GENTX_PATH/genesis.json
-sha256sum ~/.clan/config/genesis.json
+shasum -a 256 $GENTX_PATH/genesis.json
+shasum -a 256 ~/.clan/config/genesis.json
